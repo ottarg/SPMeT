78ba718195aef84c7041cab60988029ca8a4e457
Ottar Gislason <ottar.gislason@gmail.com>
2024-03-05 16:08:58 -0600

    removing junk

--- run_spme.m
+++ run_spme.m
@@ -22,42 +22,19 @@ time = 0:0.001:120;
 current = 0.*time-10;
 current(1:find(time>10,1,"first")) = 0;
 current(find(time>40,1,"first"):end) = 0;
-temperature = 0.*time+20;
+temperature = 0.*time+30;
 tic
-[res60,X] = model.simulate(time,current,temperature);
-temperature = 0.*time+10;
-% [res10,~] = model.simulate(time,current,temperature);
-toc
-figure
-hold on
-plot(res60.timeODE,res60.V)
-plot(res60.timeODE,res60.OCV)
-% plot(res10.timeODE,res10.V)
-% plot(res10.timeODE,res10.OCV)
+[res,X] = model.simulate(time,current,temperature);
 
 figure
+subplot(2,2,1)
 hold on
-plot(res60.timeODE,res60.SOC_n)
-plot(res60.timeODE,res60.SOC_p)
-
-figure
+plot(res.timeODE,res.SOC_n)
+plot(res.timeODE,res.SOC_p)
+subplot(2,2,2)
 hold on
-plot(res60.anode_potential)
-plot(res60.cathode_potential)
-% plot(res10.anode_potential)
-% plot(res10.cathode_potential)
-% 
-% 
-% %%
-% 
-% data = load('input-data/UDDS');
-% model.initial_voltage = data.voltage(1);
-% tic
-% [res,~] = model.simulate(data.time,data.current,data.temp);
-% toc
-% figure
-% hold on
-% plot(res.timeODE,res.V)
-
-figure
-plot(X(:,end))
+plot(res.anode_potential)
+plot(res.cathode_potential)
+subplot(2,2,3)
+plot(res.timeODE,res.V)
+plot(res.timeODE,res.OCV)
